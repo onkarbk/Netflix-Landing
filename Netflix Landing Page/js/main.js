06a47a86bb23fab7e2802f4ee7ce0a2647b134a7
Onkar <onkarbk1@gmail.com>
2021-05-13 21:49:48 +0530

    first commit

---
+++ Netflix Landing Page/js/main.js
@@ -0,0 +1,28 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+//Select tab content item
+function selectItem(e){
+    removeBorder();
+    removeShow();
+    //Add border to current tab
+    this.classList.add('tab-border');
+    //Grab content item from the DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // console.log(this.id);
+    //Add show class
+    tabContentItem.classList.add('show');
+}
+
+//Remove the red border at the bottom of the tab
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+//Remove the class show
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+//Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
